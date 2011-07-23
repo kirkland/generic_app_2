9b93cc2e9f6089caa8d9470ecb7c73ee14d7d528
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-07-23 11:50:37 -0400

    misc again

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Logged in!"
     else
-      flash.now.alert = "Invalid email or password"
+      flash.alert = "Invalid email or password"
       redirect_to sign_in_path
     end
   end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,2 @@
 class WelcomeController < ApplicationController
-  def index
-    render :text => "current_user: #{@current_user.inspect}"
-  end
 end
